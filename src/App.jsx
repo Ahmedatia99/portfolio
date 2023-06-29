1f488ebf1ae64354ce5eb416dd159d35b43f9a22
Ahmedatia99 <atyaa629@gmail.com>
2023-06-29 17:11:02 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   }, []);
   return (
     <div>
-      {/* */}
+      {/* ROutes between pages*/}
       <Routes>
         <Route path="/" element={<DefaultLayout />}>
           <Route path="/" element={<Home />} />
