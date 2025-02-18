af9e989a0a9e1d3d74916f8e3c132440aa817842
Ahmedatia99 <atyaa629@gmail.com>
2025-02-18 09:25:13 +0200

    add main commponents Project card && update projects section

---
+++ src/components/atoms/ProjectCard.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import { EffectCoverflow, Pagination } from "swiper/modules";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import { FaArrowRightLong } from "react-icons/fa6";
+
+const ProjectCard = ({ data, limit }) => {
+  const displayData = limit ? data.slice(0, limit) : data;
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return `<span class="${className}">  </span>`;
+    },
+  };
+
+  const breakPoints = {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    420: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    767: {
+      slidesPerView: 2,
+      spaceBetween: 70,
+    },
+    1025: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    12769: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  };
+
+  return (
+    <div className="relative container mx-auto flex justify-center items-center gap-5 flex-wrap">
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 20,
+          stretch: 1,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={pagination}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+        breakpoints={breakPoints}
+      >
+        {displayData.map((item) => {
+          return (
+            <SwiperSlide
+              key={item.id}
+              className=" max-w-sm bg- border-2 border-primary rounded-lg shadow-xl"
+            >
+              <img
+                className="rounded-md w-full h-[auto] aspect-auto object-fill"
+                src={item.imageUrl}
+                alt={item.name}
+              />
+
+              <div className="p-5 bg-white rounded-b-lg text-white">
+                <h5 className="font-kanit mb-2 text-2xl font-bold tracking-tight uppercase text-primary">
+                  {item.name}
+                  <span className="text-morning text-6xl ml-1 leading-[0]">
+                    .
+                  </span>
+                </h5>
+                <p className="mb-3 font-medium h-[60px] text-gray-500">
+                  {item.description}
+                </p>
+                <Link
+                  to={item.link}
+                  className="link relative transition-all flex items-center px text-primary border-solid border-primary border-2 rounded-lg px-5 py-1 w-fit"
+                >
+                  <span className="z-30 !text-md tracking-wider font-semibold font-kanit">
+                    DEMO
+                  </span>
+
+                  <FaArrowRightLong className="ml-2 relative z-30" />
+                </Link>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+};
+
+export default ProjectCard;
